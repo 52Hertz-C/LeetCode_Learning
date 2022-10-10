4c64630ce792d420cfd636ef22ff270e963b9eb8
chenxuyang02 <chenxuyang02@58.com>
2022-10-10 19:06:21 +0800

    LeetCode 1

--- src/main/java/com/leetcode/editor/cn/T1.java
+++ src/main/java/com/leetcode/editor/cn/T1.java
@@ -25,19 +25,13 @@ public class T1{
     }
 
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            int numOne = nums[i];
-            int numOther = target-numOne;
-            for (int j = 0; j < nums.length; j++) {
-                if(nums[j]==numOther&&j!=i){
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
                 }
             }
         }
-        return result;
+        return new int[0];
     }
-
 }
