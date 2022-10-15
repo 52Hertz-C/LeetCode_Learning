a997f9af5a9b816867e7db3b39d560020b0019d9
chenxuyang02 <chenxuyang02@58.com>
2022-10-15 15:05:12 +0800

    LeetCode 5

---
+++ src/main/java/com/leetcode/editor/cn/T5.java
@@ -0,0 +1,75 @@
+package com.leetcode.editor.cn;
+
+//给你一个字符串 s，找到 s 中最长的回文子串。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "cbbd"
+//输出："bb"
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 1000
+// s 仅由数字和英文字母组成
+//
+// Related Topics 字符串 动态规划
+// 👍 5809 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class T5 {
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int left = 0, right = 0;
+        int len = 1;
+        int maxStart = 0;
+        int maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            left = i - 1;
+            right = i + 1;
+            while (left >= 0 && s.charAt(left) == s.charAt(i)) {
+                left--;
+                len++;
+            }
+            while (right < s.length() && s.charAt(right) == s.charAt(i)) {
+                right++;
+                len++;
+            }
+            while (left >= 0 && right < s.length() && s.charAt(right) == s.charAt(left)) {
+                len = len + 2;
+                left--;
+                right++;
+            }
+            if (len > maxLen) {
+                maxLen = len;
+                maxStart = left;
+            }
+            len = 1;
+        }
+
+        return s.substring(maxStart + 1, maxStart + maxLen + 1);
+    }
+
+    public static void main(String[] args) {
+        String ccc = longestPalindrome("aba");
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
