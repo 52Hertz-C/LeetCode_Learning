b5ce2e6aaeaa7014a764ea04fe6487819b241867
chenxuyang02 <chenxuyang02@58.com>
2022-10-15 15:21:07 +0800

    LeetCode 11

--- src/main/java/com/leetcode/editor/cn/T11.java
+++ src/main/java/com/leetcode/editor/cn/T11.java
@@ -1,4 +1,6 @@
-//给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, 
+package com.leetcode.editor.cn;
+
+//给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i,
 //ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。 
 //
 // 说明：你不能倾斜容器。 
@@ -55,32 +57,18 @@ public class T11 {
     }
 
     public static int maxArea(int[] height) {
-//        int maxArea = 0;
-//        for (int i = 0; i < height.length; i++) {
-//            int h1 = height[i];
-//            for (int j = i + 1; j < height.length; j++) {
-//                int h2 = height[j];
-//                int min = Math.min(h1, h2);
-//                int area = min * (j - i);
-//                if (area > maxArea) {
-//                    maxArea = area;
-//                }
-//            }
-//        }
-//        return maxArea;
-//        int maxArea = 0;
-//        int start = 0;
-//        int end = height.length - 1;
-//        for (int i = 0; i < height.length; i++) {
-//            int area = Math.min(height[start], height[end]) * (end - start);
-//            maxArea = Math.max(area, maxArea);
-//            if (height[start] > height[end]) {
-//                end--;
-//            } else {
-//                start++;
-//            }
-//        }
-//        return maxArea;
+        int start = 0;
+        int end = height.length - 1;
+        int area = 0;
+        while (start < end) {
+            area = Math.max(area, (end - start) * Math.min(height[start], height[end]));
+            if (height[start] > height[end]) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+        return area;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
