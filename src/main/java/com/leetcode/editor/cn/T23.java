ad8a3c2fcef4e7a0a0c2bd2646a31a9ec4caa711
chenxuyang02 <chenxuyang02@58.com>
2022-10-16 14:09:27 +0800

    LeetCode 23

--- src/main/java/com/leetcode/editor/cn/T23.java
+++ src/main/java/com/leetcode/editor/cn/T23.java
@@ -19,6 +19,36 @@ import com.leetcode.editor.cn.config.ListNode;
 import java.util.Arrays;
 
 public class T23{
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        ListNode node1 = lists[0];
+        if (lists.length == 1) {
+            return node1;
+        }
+        for (int i = 1; i < lists.length; i++) {
+            ListNode list = lists[i];
+            node1 = mergeTwoList(node1, list);
+        }
+        return node1;
+    }
+
+    private static ListNode mergeTwoList(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoList(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoList(l1, l2.next);
+            return l2;
+        }
+    }
+
     public static void main(String[] args) {
         ListNode[] lists = new ListNode[3];
         ListNode l1 = new ListNode(1);
@@ -38,60 +68,52 @@ public class T23{
 
         mergeKLists(lists);
     }
-    
+
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-    public static ListNode mergeKLists(ListNode[] lists) {
-        int count = 0;
-        for (ListNode listNode : lists) {
-            if(listNode!=null) {
-                while (listNode.next != null) {
-                    listNode = listNode.next;
-                    count++;
-                }
-                count++;
-            }
-        }
-        int[] list = new int[count];
-
-        int nextNum = 0;
-        int num = 0;
-        for (ListNode listNode : lists) {
-            ListNode listNode1 = listNode;
-            if(listNode!=null){
-                nextNum = num;
-                while(listNode.next!=null){
-                    listNode = listNode.next;
-                    num++;
-                }
-                num++;
-
-                for (int i = nextNum; i < num; i++) {
-                    list[i] = listNode1.val;
-                    listNode1 = listNode1.next;
-                }
-            }
-        }
-
-        Arrays.sort(list);
-
-        if(list.length!=0){
-            ListNode root = new ListNode(list[0]);
-            ListNode other = root;
-            for (int i = 1; i < list.length; i++) {
-                ListNode temp1 = new ListNode(list[i]);
-                other.next = temp1;
-                other = temp1;
-            }
-            return root;
-        }
-        return null;
-    }
+//    public static ListNode mergeKLists(ListNode[] lists) {
+//        int count = 0;
+//        for (ListNode listNode : lists) {
+//            if(listNode!=null) {
+//                while (listNode.next != null) {
+//                    listNode = listNode.next;
+//                    count++;
+//                }
+//                count++;
+//            }
+//        }
+//        int[] list = new int[count];
+//
+//        int nextNum = 0;
+//        int num = 0;
+//        for (ListNode listNode : lists) {
+//            ListNode listNode1 = listNode;
+//            if(listNode!=null){
+//                nextNum = num;
+//                while(listNode.next!=null){
+//                    listNode = listNode.next;
+//                    num++;
+//                }
+//                num++;
+//
+//                for (int i = nextNum; i < num; i++) {
+//                    list[i] = listNode1.val;
+//                    listNode1 = listNode1.next;
+//                }
+//            }
+//        }
+//
+//        Arrays.sort(list);
+//
+//        if(list.length!=0){
+//            ListNode root = new ListNode(list[0]);
+//            ListNode other = root;
+//            for (int i = 1; i < list.length; i++) {
+//                ListNode temp1 = new ListNode(list[i]);
+//                other.next = temp1;
+//                other = temp1;
+//            }
+//            return root;
+//        }
+//        return null;
+//    }
 }
