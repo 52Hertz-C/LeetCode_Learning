40372817dae387c2c256f8fcf12263596fdb3434
chenxuyang02 <chenxuyang02@58.com>
2022-10-22 15:52:59 +0800

    leetcode T49

---
+++ src/main/java/com/leetcode/editor/cn/T49/Solution.java
@@ -0,0 +1,67 @@
+package com.leetcode.editor.cn.T49;
+
+//给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+//
+// 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+//
+// 示例 2:
+//
+//
+//输入: strs = [""]
+//输出: [[""]]
+//
+//
+// 示例 3:
+//
+//
+//输入: strs = ["a"]
+//输出: [["a"]]
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= strs.length <= 10⁴
+// 0 <= strs[i].length <= 100
+// strs[i] 仅包含小写字母
+//
+//
+// Related Topics 数组 哈希表 字符串 排序 👍 1299 👎 0
+
+
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        if (strs.length == 0) {
+            return res;
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String s = new String(chars);
+            List<String> list = map.getOrDefault(s, new ArrayList<>());
+            list.add(str);
+            map.put(s, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = groupAnagrams(strs);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
